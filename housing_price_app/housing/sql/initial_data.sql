39a107cb48d14c61d8aa99373c75dc386da82e15
Harrison (Liding) Li <32115568+HarrisonLL@users.noreply.github.com>
2022-12-10 00:40:25 -0500

    Delete housing_price_app directory

--- housing_price_app/housing/sql/initial_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO city (id, city_name, city_lat, city_lng)
-VALUES 
-(1, 'Princeton,NJ', 40.3573, -74.6672),
-(2, 'West Windsor,NJ', 40.2983, -74.6186),
-(3, 'Lawrence,NJ', 40.2778, -74.7294),
-(4, 'Seattle,WA', 47.6062, -122.3321),
-(5, 'NYC,NY', 40.7128, -74.0060);
